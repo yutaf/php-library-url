92af7a7d944874d0f0636d381b34033ed968015a
yutaf <yutafuji2008@gmail.com>
2015-09-29 17:34:46 +0900

    Add Url::getAbsoluteDirname

--- src/Url.php
+++ src/Url.php
@@ -8,6 +8,18 @@ namespace Yutaf;
 class Url
 {
     /**
+     * Get absolute parent directory name
+     *
+     * @return string
+     */
+    public static function getAbsoluteDirname()
+    {
+        $scheme = self::getScheme();
+        $script_dir = dirname($_SERVER['SCRIPT_NAME']);
+        return "{$scheme}://{$_SERVER['HTTP_HOST']}{$script_dir}";
+    }
+
+    /**
      * get url scheme
      *
      * @return string
